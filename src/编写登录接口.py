980d3f844668c6b5cf3dd24a8ee649da906b0145
smartcat <1027699719@qq.com>
2019-08-22 23:15:00 +0800

    聪明毛格式化了代码

--- src/编写登录接口.py
+++ src/编写登录接口.py
@@ -1,7 +1,7 @@
 name = "聪明猫"
 password = "1234567"
 n = 3
-while(n > 0):
+while (n > 0):
     your_name = input("please input your name:")
     your_password = input("please input your password:")
     if your_name == name and your_password == password:
@@ -9,4 +9,4 @@ while(n > 0):
         break
     else:
         n = n - 1
-        print("你还剩",n,"次机会")
+        print("你还剩", n, "次机会")
