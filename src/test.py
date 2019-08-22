e80e4241e5ae9c39c1cd723bc0cc324c519fe5aa
smartcat <1027699719@qq.com>
2019-08-22 23:09:31 +0800

    '添加了代码'

---
+++ src/test.py
@@ -0,0 +1,12 @@
+name = "聪明猫"
+password = "1234567"
+n = 3
+while (n > 0):
+    your_name = input("please input your name:")
+    your_password = input("please input your password:")
+    if your_name == name and your_password == password:
+        print("欢迎进入")
+        break
+    else:
+        n = n - 1
+        print("你还剩", n, "次机会")
