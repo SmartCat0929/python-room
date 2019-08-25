98ccf01df581b2efdd4b203b2e9960a85a8e830b
smartcat <1027699719@qq.com>
2019-08-25 12:29:10 +0800

    格式化字符串“

--- src/123.py
+++ src/123.py
@@ -1,17 +1,16 @@
-num1 = 1
-
-while num1 <= 9:
-    num2 = num1
-
-    while num2 <= 9:
-
-
-        mul = num1 * num2
-        print(num1, "*", num2, " =", mul, end=" ")
-        num2 += 1
-
-    num1 += 1
-    print()
+name = input("name:")
+age = input("nage:")
+job = input("job:")
+salary = input("salary:")
+msg = '''
+--------info of %s---------
+name: %s
+age: %d
+job: %s
+salary: %d
+---------end----------
+''' % (name,name,age,job,salary)
+print (msg)
 
 
 
---
+++ src/九九乘法表.py
@@ -0,0 +1,18 @@
+num1 = 1
+
+while num1 <= 9:
+    num2 = num1
+
+    while num2 <= 9:
+
+
+        mul = num1 * num2
+        print(num1, "*", num2, " =", mul, end=" ")
+        num2 += 1
+
+    num1 += 1
+    print()
+
+
+
+
