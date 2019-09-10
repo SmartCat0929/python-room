8a8560f0ef7b7f5c6d8796f2b734a3ca2aeae2ca
smartcat <1027699719@qq.com>
2019-09-10 08:04:05 +0800

    文件

--- src/文件的增删改查.py
+++ src/文件的增删改查.py
@@ -1,18 +1,4 @@
-# import time
-#
-# f  = open("小重山2", "w",encoding="utf8")
-# data=f.write("hello \n")
-# data=f.write("hello2 \n")
-# time.sleep(30)
-# # print(data)
-# f.close()
-
 f = open("小重山","r",encoding ="utf8")
-# f1.readline()
-# print(f1.readlines())
-# print(f1.readline())
-# for i in f1.readlines():
-#     print(i.strip())
 with open("小重山", "r",encoding ="utf8") as f_read, open("小重山2", "w",encoding ="utf8")as f_write:
     number = 0
     for line in f_read:
